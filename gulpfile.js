8a69dfc9ec22c9093e6af220c7e03b964c029e5a
Dominik Krzywiecki <dominik@krzywiecki.com>
2016-02-09 17:05:35 +0100

    Modify gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,31 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var imageop = require('gulp-image-optimization');
+var livereload = require('gulp-livereload');
+
+// gulp.task('concat-styles', function () {
+//     var g = gulp.src(['./bower_components/bootstrap/dist/css/bootstrap.min.css', './styles/**/*.scss'])
+//     .pipe(concat('main.scss'))
+//     .pipe(gulp.dest('./styles'))
+//     .pipe(notify({ message: 'Style files concated'}));    
+//     return g;
+// });
+
+gulp.task('css', function () {
+    var g = gulp.src('./styles/*.css')
+    .pipe(gulp.dest( './dist/css'));
+    return g;
+});
 
 gulp.task('sass', function () {
-	var g = sass( './styles/style.scss', { style: 'compressed' })
-	.pipe(gulp.dest( './dist/css'))
-	.pipe(notify({ message: 'Finished compile SASS files'}));	
-	return g;
+    var g = sass('./styles/style.scss', { style: 'compressed' })
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(notify({ message: 'Finished compile SASS files'})); 
+    return g;
 });
 
 gulp.task('libs.js', function() {
-  var g = gulp.src(['./bower_components/jquery/dist/jquery.js', './bower_components/angular/angular.js'])
+  var g = gulp.src(['./bower_components/jquery/dist/jquery.js', './bower_components/bootstrap/dist/js/bootstrap.min.js', './bower_components/angular/angular.js', './bower_components/angular-route/angular-route.js'])
     .pipe(concat('libs.js'))
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
@@ -24,9 +39,9 @@ gulp.task('libs.js', function() {
 });
 
 gulp.task('app.js', function() {
-  var g = gulp.src('./js/*.js')
+  var g = gulp.src('./js/**/*.js')
   	.pipe(concat('app.js'))
-    .pipe(uglify())
+    //.pipe(uglify())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest( './dist/js'))
     .pipe(notify({ message: 'app.min.js created'}));
@@ -44,7 +59,7 @@ gulp.task('images', function(cb) {
 gulp.task('default', function() {
     gulp.watch( './styles/**/*.scss', ['sass']);
     gulp.watch( './js/**/*.js', ['app.js']);
-    gulp.watch( './img', ['images']);
+    gulp.watch( './img/*', ['images']);
 });
 
-gulp.task('build', ['sass', 'app.js', 'libs.js', 'images']);
+gulp.task('build', ['css', 'sass', 'app.js', 'libs.js', 'images']); 
